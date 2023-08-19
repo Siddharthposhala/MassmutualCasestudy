b0cc9e1be36a01346394eafc38e6cf16bbc36e3e
Siddharthposhala <siddharthposhala020@gmail.com>
2023-08-19 21:04:36 +0530

    Your commit message here

---
+++ lead-admin-back/lead-admin-back/src/main/java/com/example/leadadminback/LeadAdminBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.leadadminback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadAdminBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadAdminBackApplication.class, args);
+	}
+
+}
---
+++ lead-admin-back/lead-admin-back/src/main/java/com/example/leadadminback/lead/Lead.java
@@ -0,0 +1,81 @@
+package com.example.leadadminback.lead;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collection = "lead")
+public class Lead {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String status;
+    private String assignedTo;
+
+
+}
